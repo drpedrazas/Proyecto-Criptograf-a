4ecf89023bdebad879ccdd8ee36d6c922e65181e
david <drpedrazas@unal.edu.co>
2022-02-02 16:08:20 -0500

    Elliptic curves are done

---
+++ M_V.py
@@ -0,0 +1,9 @@
+from aux_prime_functions import *
+
+class M_V:
+
+    def __init__(self):
+        self.curve = self.gen_elliptic()
+        pass
+    def gen_elliptic(self):
+        pass
--- elliptic_curve.py
+++ elliptic_curve.py
@@ -1,15 +1,30 @@
 import itertools
-
+import functools
+from tokenize import group
+from aux_prime_functions import *
 class elliptic_curve:
-    def __init__(self, p, a, b):
-        if (4*a**3 + 27*b**2 ) % p != 0 and p > 3:
-            self.a = a
-            self.b = b
+    def __init__(self, p=None, a = None, b = None):
+        if p != None:
             self.p = p
-            self.equation = lambda x, y: (y**2 - x**3 - self.a*x - self.b) % p
-            self.dots = self.gen_dots()
         else:
-            print("no cumple el criterio")
+            self.p = find_prime(18)
+        if a != None and b != None:
+            if (4*a**3 + 27*b**2 ) % p != 0 and p > 3:
+                self.a = a
+                self.b = b
+            else:
+                self.a, self.b = self.gen_parameters()
+        else:
+            self.a, self.b = self.gen_parameters()
+        self.equation = lambda x, y: (y**2 - x**3 - self.a*x - self.b) % self.p
+        self.dots = dict()
+        self.cyclic = self.gen_cyclic()
+
+    def gen_parameters(self):
+        for i in range(self.p):
+            for j in range(self.p):
+                if (4 * (i ** 3) + 27 * (j ** 2)) % self.p != 0:
+                    return i, j
 
     def modular_sum(self,x,y):
         return (x + y) % self.p
@@ -23,18 +38,53 @@ class elliptic_curve:
             if self.modular_mult(i,y) == 1:
                 return self.modular_mult(x,i)
     def lamb(self,P,Q):
+        if P[0] == Q[0] and P[1] == (self.p - Q[1]):
+            return None
         if P != Q:
-            return self.modular_div(self.modular_sus(Q[1],P[1]),self.modular_sus(Q[0],P[0]))
+            return self.modular_mult(self.modular_sus(Q[1],P[1]), multiplicative_inverse(self.modular_sus(Q[0],P[0]), self.p))
         else:
             numerador = self.modular_sum(self.modular_mult(self.modular_mult(P[0],P[0]),3),self.a)
-            denominador = self.modular_mult(P[1],2)
-            print(numerador,denominador)
-            return self.modular_div(numerador, denominador)
+            denominador = multiplicative_inverse(self.modular_mult(P[1],2), self.p)
+            return self.modular_mult(numerador, denominador)
 
-    def gen_dots(self):
-        return [(x,y) for x, y in itertools.product(range(self.p),range(self.p)) if self.equation(x,y) == 0]
+    def curve_sum(self,P,Q):
+        if P == "O":
+            return Q
+        if Q == "O":
+            return P
+        l  = self.lamb(P,Q) 
+        if l == None:
+            return "O"
+        else:
+            x_3 = self.modular_sus(self.modular_sus(self.modular_mult(l, l), P[0]) , Q[0])
+            y_3 = self.modular_sus(self.modular_mult(l , (self.modular_sus(P[0] , x_3))) , P[1])
+            return x_3, y_3
 
+    def gen_group(self,Q):
+        group = [Q]
+        curr = Q
+        while curr != "O":
+            curr = self.curve_sum(curr, Q)
+            group.append(curr)
+        return group
 
-ec = elliptic_curve(11,1,6)
-print(ec.dots)
-print(ec.lamb((2,4),(3,6)))
+    def gen_cyclic(self):
+        for i in range(self.p):
+            for j in range(self.p):
+                if self.equation(i,j) == 0:
+                    initial = i, j
+                    if self.dots == dict():
+                        self.dots[initial] = self.gen_group(initial)
+                        if len(self.dots[initial]) > 10 ** 5:
+                            return self.dots[initial]
+                        else:
+                            continue
+                    if initial in self.dots:
+                        continue
+                    elif functools.reduce(lambda x,y: x or y, [initial in self.dots[i] for i in self.dots]):
+                        continue
+                    else:
+                        self.dots[initial] = self.gen_group(initial)
+                        if len(self.dots[initial]) > 10 ** 5:
+                            return self.dots[initial]
+                        continue
